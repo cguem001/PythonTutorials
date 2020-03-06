044239984ec76e04a2db8db5a99e71548ce562c1
cguem001 <47282961+cguem001@users.noreply.github.com>
2020-03-06 16:23:49 +0000

    Add files via upload

---
+++ 4.ProgramFlowControl/capitalletterschallenge.py
@@ -0,0 +1,10 @@
+quote = """
+Alright, but apart from the Sanitation, the Medicine, Education, Wine,
+Public Order, Irrigation, Roads, the Fresh-Water System,
+and Public Health, what have the Romans ever done for us?
+"""
+capitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+for char in quote:
+    if char in capitalLetters:
+        print(char)
