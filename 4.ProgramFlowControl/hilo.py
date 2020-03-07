01152900a46cd23fb96f651477bb97773263c0b5
cguem001 <47282961+cguem001@users.noreply.github.com>
2020-03-07 22:16:53 +0000

    Personal laptop

--- 4.ProgramFlowControl/hilo.py
+++ 4.ProgramFlowControl/hilo.py
@@ -4,21 +4,24 @@ high = 1000
 print("Please think of a number between {} and {}".format(low, high))
 input("Press ENTER to start")
 
-guess = 1
+guesses = 1
 while True:
+    print("\tGuessing in the range of {} to {}".format(low, high))
     guess = low + (high - low) // 2
-    high_low = input("My guess is {}. Should I get higher or lower? Enter h or l, or c if my guess was correct".format(
+    high_low = input("My guess is {}. Should I get higher or lower?"
+                     "Enter h or l, or c if my guess was correct ".format(
         guess).casefold())
-
 
     if high_low == "h":
         # Guess higher. The low end of the range becomes 1 greater than the guess
-        pass
+        low = guess + 1
     if high_low == "l":
         # Guess lower. The high end of the range becomes one less than the guess
-        pass
+        high = guess - 1
     elif high_low == "c":
-        print("I got it in {} guesses!".format(guess))
+        print("I got it in {} guesses!".format(guesses))
+        break
     else:
         print("Please enter h, l or c")
 
+    guesses += 1
