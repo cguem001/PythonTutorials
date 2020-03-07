54e247a528ebe34e2b99116814d7869b15c709a4
cguem001 <47282961+cguem001@users.noreply.github.com>
2020-03-07 16:36:14 +0000

    Personal laptop

--- 4.ProgramFlowControl/guessinggame.py
+++ 4.ProgramFlowControl/guessinggame.py
@@ -1,18 +1,38 @@
-answer = 5
+import random
 
-print("Please guess number between 1 and 10: ")
-guess = int(input())
+higher = 10
+answer = random.randint(1, 10)
+print("The answer is {}".format(answer))  # TODO: Remove after testing
 
-if guess == answer:
-    print("You got it first time")
-else:
-    if guess < answer:
-        print("Pleas guess higher")
-    else:   # guess must be greater than answer
-        print("Please guess lower")
+print("Please guess number between 1 and {}: ".format(higher))
+print("If you press zero the game will be over")
+guess = 0
+
+while guess != answer:
     guess = int(input())
+    if guess == 0:
+        print("Game over")
+        break
     if guess == answer:
         print("Well done, you guessed it")
+        break
     else:
-        print("Sorry, you have not guessed correctly")
+        if guess < answer:
+            print("Guess higher")
+        if guess > answer:
+            print("Guess lower")
 
+
+# if guess == answer:
+#     print("You got it first time")
+# else:
+#     if guess < answer:
+#         print("Please guess higher")
+#     else:   # guess must be greater than answer
+#         print("Please guess lower")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done, you guessed it")
+#     else:
+#         print("Sorry, you have not guessed correctly")
+#
