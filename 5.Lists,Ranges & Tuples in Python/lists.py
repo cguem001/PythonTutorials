9a8ace96edcc8c9b299bc3290ebb8bacb68435e3
Carlos Guemes <carlosguemes.martinez@ynap.com>
2020-03-09 12:54:57 +0000

    Another try

--- 5.Lists,Ranges & Tuples in Python/lists.py
+++ 5.Lists,Ranges & Tuples in Python/lists.py
@@ -1,26 +1,71 @@
-# ipAddress = input("Please enter an IP address: ")
-# print(ipAddress.count("."))
-parrot_list = ["non pinin'", "no more", "a stiff", "bereft of live"]
+# # ipAddress = input("Please enter an IP address: ")
+# # print(ipAddress.count("."))
+# parrot_list = ["non pinin'", "no more", "a stiff", "bereft of live"]
+#
+# parrot_list.append("A Norwegian Blue")
+#
+# for state in parrot_list:
+#     print("This parrot is " + state)
+#
+# even = [2, 4, 6, 8]
+# odd = [1, 3, 5, 7, 9]
+#
+# numbers = even + odd
+# numbers_in_order = sorted(numbers)
+# print(numbers_in_order)
+#
+# if numbers == numbers_in_order:
+#     print("The lists are equal")
+# else:
+#     print("The lists are not equal")
+#
+# if numbers_in_order == sorted(numbers):
+#     print("The lists are equal")
+# else:
+#     print("The lists are not equal")
 
-parrot_list.append("A Norwegian Blue")
+# list_1 = []
+# list_2 = list()
+#
+# print("List 1: {}".format(list_1))
+# print("List 2: {}".format(list_2))
+#
+# if list_1 == list_2:
+#     print("The lists are equal")
+#
+# print(list("The lists are equal"))
 
-for state in parrot_list:
-    print("This parrot is " + state)
+# even = [2, 4, 6, 8]
+# another_even = list(even)
+# print(another_even is even)
+# another_even.sort(reverse=True)
+# print(even)
 
-even = [2, 4, 6, 8]
-odd = [1, 3, 5, 7, 9]
+# even = [2, 4, 6, 8]
+# odd = [1, 3, 5, 7, 9]
+#
+# numbers = [even, odd]
+# print( numbers)
+#
+# for number_set in numbers:
+#     print(number_set)
+#
+#     for value in number_set:
+#         print(value)
 
-numbers = even + odd
-numbers_in_order = sorted(numbers)
-print(numbers_in_order)
+menu = []
+menu.append(["egg", "spam", "bacon"])
+menu.append(["egg", "sausage", "bacon"])
+menu.append(["egg", "spam"])
+menu.append(["egg", "bacon", "spam"])
+menu.append(["egg", "bacon", "sausage", "spam"])
+menu.append(["spam", "bacon", "sausage", "spam"])
+menu.append(["spam", "egg", "spam", "spam", "bacon", "spam"])
+menu.append(["spam", "egg", "sausage", "spam"])
 
-if numbers == numbers_in_order:
-    print("The lists are equal")
-else:
-    print("The lists are not equal")
 
-if numbers_in_order == sorted(numbers):
-    print("The lists are equal")
-else:
-    print("The lists are not equal")
-
+for meal in menu:
+    if not "spam" in meal:
+        print(meal)
+        for ingredient in meal:
+            print(ingredient)
